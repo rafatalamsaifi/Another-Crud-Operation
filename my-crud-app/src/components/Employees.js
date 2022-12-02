9529686afd8587911b52bdbbae22545d5c4868cc
rafatalamsaifi <rafatalamsaifi@gmail.com>
2022-12-02 12:51:57 +0000

    initial commit

---
+++ my-crud-app/src/components/Employees.js
@@ -0,0 +1,20 @@
+const Employee = [
+    {
+      id: 1,
+      Name: "Rafat",
+      Age: 30,
+    },
+    {
+      id: 2,
+      Name: "Alam",
+      Age: 20,
+    },
+    {
+      id: 3,
+      Name: "Rafat Alam Saifi",
+      Age: 25,
+    },
+  ];
+  
+  export default Employee;
+  
